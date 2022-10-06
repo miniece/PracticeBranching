3ee68581750b6505d2f99b763bbf660299a5a38c
miniece <mvelinda@icloud.com>
2022-10-06 09:10:35 -0400

    Practice branching i think

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+Console.WriteLine("Welcome to Grand Circus' Room Detail Generator!");
+
+Console.WriteLine("Enter Length:");
+double length = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Width:");
+double width = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Area:");
+double area = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Perimeter:");
+double perimeter = double.Parse(Console.ReadLine());
+
+if (area <= 250)
+{
+    Console.WriteLine("This is a smaller room.");
+}
+else if (area >= 650)
+{
+    Console.WriteLine("This is a Larger room.");
+}
+else
+{
+    Console.WriteLine("This is a medium-sized room.");
+}
+
+Console.WriteLine("Thank you for using the Room Detail Generator");
+
